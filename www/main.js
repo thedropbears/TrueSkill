45cf4cf927a7ac8d587f906687e55879884c030c
Joel Bryla <joel@wiredlizard.com>
2017-03-13 13:54:23 +1100

    added card generation and editing fuctions

--- www/main.js
+++ www/main.js
@@ -2,7 +2,7 @@ function Donut_chart(options) {
 	
 	this.settings = $.extend({
 		element: options.element,
-		percent: 100
+		percent: 100	
 	}, options);
 	
 	
@@ -39,10 +39,69 @@ function Donut_chart(options) {
 	}
 };
 
+function make_card(red_odds, teams, match_name, match_location, ts, type){
+	var blue_team = "blue-team"
+	var red_team = "red-team"
+
+		if (type === 'match_blue'){
+			blue_team = "blue-team-won"
+		}
+		else if (type === 'match_red'){
+			red_team = "red-team-won"
+		}
+		
+		return "<div class=\"match-card mdl-card mdl-shadow--4dp card "+ts+"\"> \
+            <div class=\"mdl-card__title white-red\"> \
+              <h2 class=\"mdl-card__title-text\">"+match_name+" - "+ match_location +"</h2> \
+            </div>\
+            <div class=\"mdl-card__supporting-text team\">\
+              <div class=\'"+blue_team+" "+ts+"\'>\
+                <ul>\
+                  <li>"+String(100-red_odds)+"%</li>\
+                  <li>"+teams[0]+"</li>\
+                  <li>"+teams[1]+"</li>\
+                  <li>"+teams[2]+"</li>\
+                </ul>\
+              </div>\
+              <div class=\'"+red_team+" "+ts+"\'>\
+                <ul>\
+                  <li>"+String(red_odds)+"%</li>\
+                  <li>"+teams[0]+"</li>\
+                  <li>"+teams[1]+"</li>\
+                  <li>"+teams[2]+"</li>\
+                </ul>\
+              </div>\
+            </div>\
+          </div>"
+}
+
+function change_card(win, ts){
+	console.log("helo")
+	if (win === 'red'){
+		$('.'+ts+'.red-team').removeClass('red-team').addClass("red-team-won")
+	}
+	else if (win === 'blue'){
+		console.log('.'+ts+'.blue-team')
+		$('.'+ts+'.blue-team').removeClass('blue-team').addClass("blue-team-won")
+
+	}}
+
+
+var events = []
+
 $(function() {
 	$('.donut-chart').each(function(index) {
 		$(this).append('<svg preserveAspectRatio="xMidYMid" xmlns:xlink="http://www.w3.org/1999/xlink" id="donutChartSVG'+index+'"><path d="M100,100" /></svg>');
 		var p = new Donut_chart({element: $('#donutChartSVG'+index), percent: $(this).attr('data-percent')});
 		p.animate();
 	});
+
+if (events != []){
+for (event in events){	
+	$("#event-selector").append('<option>'+events[event].short_name+'</option>');
+}}
+
+        $("#team-input").focusout(function(){
+			var team = $("#team-input").val() 
+        });
 });
